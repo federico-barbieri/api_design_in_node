86f29fe339bbb4cfcc043ccc43d0e75c52417ce0
Federico Barbieri <federicobarbieri54@gmail.com>
2024-10-11 23:51:24 +0200

    middleware

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,9 @@ const router = Router()
 PRODUCT
 */
 
-router.get("/product", () => {})
+router.get("/product", (req, res) => {
+    res.json({message: 'sup'})
+})
 router.get("/product/:id", () => {})
 router.put("/product/:id", () => {})
 router.post("/product/", () => {})
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import router from "./router"
+import morgan from "morgan"
 
 const app = express()
 
+app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
-    console.log("hello from express")
+    console.log("hello from expresssss")
     res.status(200)
     res.json({message: 'hello'})
 })
