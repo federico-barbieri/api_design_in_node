a1faaf703cc46532ac36568af5e68ce41e6293d2
Federico Barbieri <federicobarbieri54@gmail.com>
2024-10-11 23:33:13 +0200

    create routers

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import router from "./router"
 
 const app = express()
 
@@ -6,7 +7,8 @@ app.get('/', (req, res) => {
     console.log("hello from express")
     res.status(200)
     res.json({message: 'hello'})
-
 })
+
+app.use('/api', router)
 
 export default app
