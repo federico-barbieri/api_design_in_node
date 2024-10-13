ddd5000075406269e1b2cfc12946efdeda53a467
Federico Barbieri <federicobarbieri54@gmail.com>
2024-10-13 17:11:22 +0200

    protect routes and validate jwt tokens

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import router from "./router"
 import morgan from "morgan"
+import { protect } from "./modules/auth"
+
 
 const app = express()
 
@@ -14,6 +16,6 @@ app.get('/', (req, res) => {
     res.json({message: 'hello'})
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app
