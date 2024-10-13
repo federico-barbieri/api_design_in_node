dae60cd407763d515d5792aa428bf0100a91b2d6
Federico Barbieri <federicobarbieri54@gmail.com>
2024-10-13 17:36:50 +0200

    validate user

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -7,8 +7,29 @@ export const createNewUser = async (req, res) => {
             password: await hashPassword(req.body.password)
         }
     )}
+
+    const token = createJWT(user)
+    res.json({ token: token})
+
 }
 
-const token = createJWT(user)
 
-res.json({ token: token})
+export const signin = (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            username: req.body.username
+        }
+    })
+
+    const isValid = awaite comparePasswords(req.body.password, user.password){
+        if(!isValid){
+            res.status(401)
+            res.json({message: 'Not authorized'})
+            return
+        }
+
+        const token = createJWT(user){
+            res.json({token})
+        }
+    }
+}
