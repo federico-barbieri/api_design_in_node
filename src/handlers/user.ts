b0e0704d6f9bbd43f33ecfc35078a4cdcc2fffef
Federico Barbieri <federicobarbieri54@gmail.com>
2024-10-13 17:55:56 +0200

    add user route

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,12 +1,13 @@
 import prisma from "../db"
+import {comparePasswords, createJWT, hashPassword} from "../modules/auth"
 
 export const createNewUser = async (req, res) => {
-    const user = await prisma.user.create{(
+    const user = await prisma.user.create({
         data: {
             username: req.body.username,
             password: await hashPassword(req.body.password)
         }
-    )}
+    })
 
     const token = createJWT(user)
     res.json({ token: token})
@@ -14,22 +15,18 @@ export const createNewUser = async (req, res) => {
 }
 
 
-export const signin = (req, res) => {
+export const signin = async (req, res) => {
     const user = await prisma.user.findUnique({
         where: {
             username: req.body.username
         }
     })
 
-    const isValid = awaite comparePasswords(req.body.password, user.password){
+    const isValid = await comparePasswords(req.body.password, user.password)
         if(!isValid){
             res.status(401)
             res.json({message: 'Not authorized'})
             return
         }
 
-        const token = createJWT(user){
-            res.json({token})
-        }
     }
-}
