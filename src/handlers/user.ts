9c893618a270fdf24897a13a027c75658b2e8439
Federico Barbieri <federicobarbieri54@gmail.com>
2024-10-13 17:25:58 +0200

    creating a user

---
+++ src/handlers/user.ts
@@ -0,0 +1,14 @@
+import prisma from "../db"
+
+export const createNewUser = async (req, res) => {
+    const user = await prisma.user.create{(
+        data: {
+            username: req.body.username,
+            password: await hashPassword(req.body.password)
+        }
+    )}
+}
+
+const token = createJWT(user)
+
+res.json({ token: token})
